30ab20a131453a7013f26023129135798d033230
saswat <saswatranjan0602@gmail.com>
2023-11-26 21:09:08 +0530

    working on layout of the app  still in progresss

---
+++ components/ListItem.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  return <div></div>;
+};
+
+export default ListItem;
