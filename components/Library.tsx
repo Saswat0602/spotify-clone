6ee6b679f8d3610be4e45548f1b95f490d3f7a65
saswat <saswatranjan0602@gmail.com>
2023-12-02 22:56:51 +0530

    update the sidebar searchcontent and liked content

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,8 @@ import { Song } from "@/types";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
+import useOnPlay from "@/hooks/useOnPlay";
+
 import MediaItem from "./MediaItem";
 
 interface LibraryProps {
@@ -14,20 +16,26 @@ interface LibraryProps {
 }
 
 const Library: React.FC<LibraryProps> = ({ songs }) => {
-  const { user } = useUser();
+  const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
   const authModal = useAuthModal();
+
+  const onPlay = useOnPlay(songs);
+
   const onClick = () => {
     if (!user) {
       return authModal.onOpen();
     }
 
+   
+
     return uploadModal.onOpen();
   };
+
   return (
     <div className="flex flex-col">
-      <div className="flex items-center justify-between px-5 py-4">
-        <div className="inline-flex  items-center gap-x-2">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
           <p className="text-neutral-400 font-medium text-md">Your Library</p>
         </div>
@@ -45,7 +53,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
           <MediaItem
-            onClick={()=>{console.log("object")} }
+            onClick={(id: string) => onPlay(id)}
             key={item.id}
             data={item}
           />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,20 +1,26 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import Box from "./Box";
-import SidebarItem from "./SidebarItem";
-import Library from "./Library";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+
+import SidebarItem from "./SidebarItem";
+import Box from "./Box";
+import Library from "./Library";
+import { useMemo } from "react";
 
 interface SidebarProps {
   children: React.ReactNode;
   songs: Song[];
 }
-const Sidebar: React.FC<SidebarProps> = ({ children,songs }) => {
+
+const Sidebar = ({ children, songs }: SidebarProps) => {
   const pathname = usePathname();
+  const player = usePlayer();
 
   const routes = useMemo(
     () => [
@@ -35,7 +41,15 @@ const Sidebar: React.FC<SidebarProps> = ({ children,songs }) => {
   );
 
   return (
-    <div className="flex h-full">
+    <div
+      className={twMerge(
+        `
+        flex 
+        h-full
+        `,
+        player.activeId && "h-[calc(100%-80px)]"
+      )}
+    >
       <div
         className="
           hidden 
@@ -56,11 +70,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children,songs }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          <Library songs = {songs}/>
+          <Library songs={songs} />
         </Box>
       </div>
       <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
+
 export default Sidebar;
