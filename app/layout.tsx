dd4f55fab864bdb96cb0a82b2713e8a622028990
saswat <saswatranjan0602@gmail.com>
2023-12-02 22:50:57 +0530

    play the song and add various hooks  for play the song

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
 
 const font = Figtree({ subsets: ["latin"] });
 export const revalidate = 0;
@@ -27,6 +28,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <UserProvider>
               <ModalProvider>
                 <Sidebar songs={userSongs}>{children}</Sidebar>
+                <Player/>
               </ModalProvider>
             </UserProvider>
           </SupabaseProvider>
